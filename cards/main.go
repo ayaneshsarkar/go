1ce8bc77c999f85343e7f949b718befa351b85de
Ayanesh Sarkar <ayaneshsarkar101@gmail.com>
2020-10-29 15:38:57 +0530

    Initial Commit :heavy_check_mark:

--- cards/main.go
+++ cards/main.go
@@ -3,10 +3,6 @@ package main
 //import "fmt"
 
 func main() {
-	cards := newDeck()
-
-	hand, remainingCards := deal(cards, 5)
-
-	hand.print()
-	remainingCards.print()
+	cards := newDeckFromFile("cards")
+	cards.print()
 }
