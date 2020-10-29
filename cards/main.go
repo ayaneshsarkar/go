4eac3bae35490252bfb4780776e2c1c411a814a6
Ayanesh Sarkar <ayaneshsarkar101@gmail.com>
2020-10-29 12:10:23 +0530

    Initial Commit :heavy_check_mark:

--- cards/main.go
+++ cards/main.go
@@ -3,12 +3,7 @@ package main
 //import "fmt"
 
 func main() {
-	cards := deck{"Ace of Diamonds", newCard()}
-	cards = append(cards, "Seven of Diamonds")
+	cards := newDeck()
 
 	cards.print()
-}
-
-func newCard() string {
-	return "Five of Diamonds"
 }
