51608ce6d2fe24aa4e69f40dbc7d210d6ecb0d1c
Ayanesh Sarkar <ayaneshsarkar101@gmail.com>
2020-11-03 16:50:13 +0530

    Initial Commit :heavy_check_mark:

--- channels/main.go
+++ channels/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -10,22 +11,35 @@ func main() {
 		"http://google.com",
 		"http://facebook.com",
 		"http://twitter.com",
-		"http://amazon.ina",
+		"http://amazon.in",
 		"http://devayanesh.com",
 	}
 
+	c := make(chan string)
+
 	for _, link := range links {
-		go checkLink(link)
+		go checkLink(link, c)
 	}
+
+	for l := range c {
+		go func(link string) {
+			time.Sleep(5 * time.Second)
+			fmt.Println("Channel Loop")
+			checkLink(link, c)
+		}(l)
+	}
+
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 
 	if err != nil {
 		fmt.Println(link, "is down!")
+		c <- link
 		return
 	}
 
 	fmt.Println(link, "is up!")
+	c <- link
 }
